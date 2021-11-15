f29403c2d03a1d584bd49957d86fb52f50c5905b
coder-rabbani <thegolamrabbani@gmail.com>
2021-11-15 23:51:17 +0600

    code updated

--- src/Pages/SingleService/SingleService.js
+++ src/Pages/SingleService/SingleService.js
@@ -2,6 +2,7 @@ import { Container, Grid, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router';
 
 const SingleService = () => {
@@ -14,6 +15,12 @@ const SingleService = () => {
     }, []);
 
     const {img, title, description, price} = product;
+
+    const { register, handleSubmit, reset } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+        reset();
+    };
     return (
         <Container sx={{py:7}}>
             <Grid container spacing={2}>
@@ -24,6 +31,18 @@ const SingleService = () => {
                 <Typography variant="body2">{description}</Typography>
                 </Grid>
                 <Grid item xs={6} md={4}>
+                <Typography variant="h4" sx={{textAlign:"center"}}>Place Order</Typography>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <input className="input-field" placeholder="Your Name" {...register("name")} />
+                        <br />
+                        <input type="email" className="input-field" placeholder="Your Email" {...register("email")} />
+                        <br />
+                        <input className="input-field" placeholder="Address" {...register("address")} />
+                        <br />
+                        <input className="input-field" type="number" placeholder="Phone No" {...register("phone")} />
+                        
+                        <input className="input-field my-btn" type="submit" value="Place Order"/>
+                    </form>
                 </Grid>
             </Grid>
         </Container>
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -76,7 +76,7 @@ const useFirebase = () => {
 
     //admin user setting
     useEffect(()=>{
-      fetch(`http://localhost:5000/users/${user.email}`)
+      fetch(`https://drone-server-bd.herokuapp.com/users/${user.email}`)
       .then(res=>res.json())
       .then(data=>setAdmin(data.admin))
     }, [user.email]);
@@ -94,7 +94,7 @@ const useFirebase = () => {
     //save user info to database
     const saveUserToDb = (email, displayName, method)=>{
         const user = {email, displayName}
-        fetch('http://localhost:5000/users', {
+        fetch('https://drone-server-bd.herokuapp.com/users', {
           method: method,
           headers: {
             'content-type' : 'application/json'
